1e0056b77aa1db314ba4df0e4b09ed4afc052d8d
girogi <126319258+linux-giorgi@users.noreply.github.com>
2024-09-02 10:59:44 +0400

    Add files via upload

---
+++ piano-code-on-C-lang/headers.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <string.h>
---
+++ piano-code-on-C-lang/main.c
@@ -0,0 +1,32 @@
+#include "headers.h"
+void plays(const char *note) {
+    char com[100];
+    snprintf(com, sizeof(com), "play -q %s.wav > /dev/null 2>&1 &", note); 
+    system(com);
+}
+int main() {
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    printw("Press keys to play notes: C, D, E, F, G, A, B\n");
+    printw("Press 'q' to quit.\n");
+    refresh();
+    int ch;
+    while((ch = getch()) != 'q') {
+        switch(ch){
+            case '1':plays("C");break;
+            case '2':plays("D");break;
+            case '3':plays("E");break;
+            case '4':plays("F");break;
+            case '5':plays("G");break;
+            case '6':plays("A");break;
+            case '7':plays("B");break;
+            default:printw("Use C, D, E, F, G, A, B or q to quit.\n");
+                refresh();
+                break;
+        }
+    }
+    endwin();
+    return 0;
+}
