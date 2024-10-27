616e57a818c00dbdb91b2e6e34e00ad4967bb0f7
girogi <126319258+linux-giorgi@users.noreply.github.com>
2024-10-27 16:36:26 +0400

    Update main.c
    update

--- call-assembly-in-C/main.c
+++ call-assembly-in-C/main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+
 int addNumbers(int a, int b);
-int main(){
-	printf("rum of 5+5= %d\n",addNumbers(5,5));
+
+int main()
+{
+	printf("rum of 5+5= %d\n", addNumbers(5, 5));
 	return 0;
 }
