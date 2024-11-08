32e83288acce91bfc36701396464ab7e4c0d6b92
girogi <126319258+linux-giorgi@users.noreply.github.com>
2024-11-08 17:06:35 +0400

    Update main_v1.0.c

--- hex-editor-in-c.c/main_v1.0.c
+++ hex-editor-in-c.c/main_v1.0.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-void print_hex(unsigned char* buffer, int num)
-{
-	for (int i = 0; i < num; i++)
-	{
-		if (i % 10 == 0)
-		{
+void print_hex(unsigned char* buffer, int num){
+	for (int i = 0; i < num; i++){
+		if (i % 10 == 0){
 			printf("\n");
 		}
 		
@@ -14,28 +11,21 @@ void print_hex(unsigned char* buffer, int num)
 	}
 	printf("\n");
 }
-void main(int argc, char** argv)
-{
+void main(int argc, char** argv){
 	FILE* f = fopen(argv[ 1 ], "r");
 	unsigned char buffer[ 1024 ];
 	int num_of_bytes = fread(buffer, 1, 1024, f);
 	print_hex(buffer, num_of_bytes);
 
-	while(true)
-	{
+	while(true){
 		char cmd;
 		int loc;
 		scanf("%c%d", &cmd, &loc);
-		if (cmd == 'p')
-		{
+		if (cmd == 'p'){
 			print_hex(buffer + loc,10);
-		}
-		if (cmd == 'e')
-		{
+		}if (cmd == 'e'){
 			scanf("%hhx",buffer+loc);
-		}
-		if (cmd == 's')
-		{
+		}if (cmd == 's'){
 			break;
 		}
 	}
