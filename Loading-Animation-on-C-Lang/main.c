339ca46460258f09fb1dd3b95d9587b714ddb732
girogi <126319258+linux-giorgi@users.noreply.github.com>
2025-01-14 21:03:10 +0400

    Update main.c

--- Loading-Animation-on-C-Lang/main.c
+++ Loading-Animation-on-C-Lang/main.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char string[]
-    = "------------------------------------------------------------------------"
-      "----------------------------------------------------------";
+char string[] = "------------------------------------------------------------------------"
+                "----------------------------------------------------------";
 
 // size of string variable
-short size = sizeof (string) / sizeof (string[0]);
+short size = sizeof(string) / sizeof(string[0]);
 
 // animation function
-void
-animation ()
-{
-
-  // for loop for fill string list witch " # "
-  for (short i; i < size; i++)
-    {
-      system ("sleep 0.5");
-      system ("clear");
-      string[i] = '#';
-      printf ("%s\n", string);
+void animation() {
+    // for loop to fill string list with " # "
+    for (short i = 0; i < size; i++) {
+        system("sleep 0.5");
+        system("clear");
+        string[i] = '#';
+        printf("%s\n", string);
     }
 }
 
-int
-main ()
-{
-  // start
-  animation ();
-  return 0;
+int main() {
+    // start
+    animation();
+    return 0;
 }
