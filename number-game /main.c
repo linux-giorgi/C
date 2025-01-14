68977e8e166a7e27bc2b5b389edbcb3161a12b65
girogi <126319258+linux-giorgi@users.noreply.github.com>
2025-01-14 20:49:23 +0400

    Update main.c

--- number-game /main.c
+++ number-game /main.c
@@ -2,64 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// functions headers
-int number (int x, int y);
-void result (int z, int o);
 
-struct main_variables
-{
-  int first;
-  int last;
-  int num;
-  int res;
+// function headers
+int number(int x, int y);
+void result(int z, int o);
+
+struct main_variables {
+    int first;
+    int last;
+    int num;
+    int res;
 } Var;
 
-int
-main ()
-{
-  printf ("enter first value: ");
-  scanf ("%d", &Var.first);
-  printf ("enter last value: ");
-  scanf ("%d", &Var.last);
-  result (Var.first, Var.last);
-  return 0;
-} // main
-
-int
-number (int x, int y)
-{
-  srand (time (NULL));
-  x = (rand () % (y - x + 1)) + x;
-  return x;
+int main() {
+    printf("enter first value: ");
+    scanf("%d", &Var.first);
+    printf("enter last value: ");
+    scanf("%d", &Var.last);
+    result(Var.first, Var.last);
+    return 0;
 }
 
-void
-result (int z, int o)
-{
-  Var.num = number (z, o);
-  while (true)
-    {
-      printf ("enter: ");
-      scanf ("%d", &Var.res);
-      if (Var.num == Var.res)
-        {
-          puts ("yes it is!!!");
-          break;
-        }
-      else if (Var.num < Var.res)
-        {
-          puts ("high");
-          continue;
-        }
-      else if (Var.num > Var.res)
-        {
-          puts ("low");
-          continue;
-        }
-      else
-        {
-          puts ("try again");
-          continue;
+int number(int x, int y) {
+    srand(time(NULL));
+    x = (rand() % (y - x + 1)) + x;
+    return x;
+}
+
+void result(int z, int o) {
+    Var.num = number(z, o);
+    while (true) {
+        printf("enter: ");
+        scanf("%d", &Var.res);
+        if (Var.num == Var.res) {
+            puts("yes it is!!!");
+            break;
+        } else if (Var.num < Var.res) {
+            puts("high");
+        } else if (Var.num > Var.res) {
+            puts("low");
+        } else {
+            puts("try again");
         }
     }
 }
