d132fdb9a5a47f9ca5e519dc582f692f6aa9e3a0
giorgi ghurtskaia <giorgighu999@gmail.com>
2025-02-08 16:57:45 +0400

    refactored bubble sort code

--- list_sort_algorithms/bubble_sort/main.c
+++ list_sort_algorithms/bubble_sort/main.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    // some list
-    int list[] = {15, 122, 411, 12, 3545, 324, 455, 234, 52, 125, 6, 7,
-                  4, 115, 434, 55174, 315, 876, 85, 87, 67, 86, 56, 87};
-    int len = sizeof(list) / sizeof(list[0]);
-    
-    for (int lol = 0; lol < len; lol++) {
-        short count = 0;
-        count++;
-        for (int j = 0; j < len - 1; j++) {
-            int bon = j + 1;
-            if (list[j] > list[bon]) {
-                int tmp = list[j];
-                list[j] = list[bon];
-                list[bon] = tmp;
+int list[] = {15, 122, 411, 12, 3545, 324, 455, 234, 52, 125, 6, 7,
+              4, 115, 434, 55174, 315, 876, 85, 87, 67, 86, 56, 87};
+
+
+void list_sort(int list[], int size){
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (list[j] > list[j + 1]) {
+                int temp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = temp;
             }
         }
     }
-    
-    // print sorted list
-    for (int i = 0; i < len; i++) {
+
+}
+
+
+
+void list_print(int list[], int size){
+    for (int i = 0; i < size; i++) {
         printf("%i\n", list[i]);
     }
+
+}
+
+
+int main() {
+    int size = sizeof(list) / sizeof(list[0]);
+    
+	list_sort(list, size);    
+	list_print(list, size);
     
     return 0;
 }
