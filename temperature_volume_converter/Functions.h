3353885c1f1740cf816ece5c645e5e3672dc0926
giorgi ghurtskaia <giorgighu999@gmail.com>
2025-03-10 22:16:48 +0400

    refactored printf function

--- temperature_volume_converter/Functions.h
+++ temperature_volume_converter/Functions.h
@@ -1,18 +1,18 @@
 #include<stdio.h>
 
-void ListPrintFunction(char inp[][50], int rows) {
+void ListPrintFunction(char input[][50], int rows) {
     for (int i = 0; i < rows; i++) {
-        printf("%s\n", inp[i]); 
+        printf("%s\n", input[i]); 
     }
 } 
 
 void Display_menu(void) {
-	printf("\n=======| Temperature_volume_conversion_CLI_program |========\n");
-	printf("1. Celsius to something else \n");
-	printf("2. Fahrenheit to something else \n");
-	printf("3. Kelvin to something else \n");
-	printf("4. Newton to something else \n");
-	printf("5. Romer to something else \n");
-	printf("6. Reaumur to something else \n");
-	printf("0. exit \n");
+	printf("\n=======| Temperature_volume_conversion_CLI_program |========\n"
+		  "1. Celsius to something else \n"
+		  "2. Fahrenheit to something else \n"
+		  "3. Kelvin to something else \n"
+		  "4. Newton to something else \n"
+		  "5. Romer to something else \n"
+		  "6. Reaumur to something else \n"
+		  "0. exit \n");
 }
